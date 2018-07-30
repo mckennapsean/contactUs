3e015b0a5e6e53d141e3d9c3cddb7c93983c3bb4
Shi Mei Wong <wshimei@mgmail.com>
2018-07-31 00:41:20 +0800

    minimal

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomepageComponent },
+  { path: 'contact-us', component: ContactUsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
